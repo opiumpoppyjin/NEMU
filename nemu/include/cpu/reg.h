fed1695ed62b611d2171e5dd4120d09875c2928e
tracer-ics2015 <tracer@njuics.org>
2016-03-28 16:42:34 +0800

    >  compile NEMU 141220000 toxic Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:18:00 UTC 2015 i686 i686 i686 GNU/Linux  16:42:34 up 18 min,  5 users,  load average: 0.00, 0.02, 0.05 fa544f3393233c24e066315ca42553ce117c440

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -14,7 +14,7 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
-typedef struct {
+typedef union {
 	union {
 		uint32_t _32;
 		uint16_t _16;
@@ -22,10 +22,11 @@ typedef struct {
 	} gpr[8];
 
 	/* Do NOT change the order of the GPRs' definitions. */
+	struct{
+		uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 
-	uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-
-	swaddr_t eip;
+		swaddr_t eip;
+	};
 
 } CPU_state;
 
