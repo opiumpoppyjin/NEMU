96de13f5554e15d75aca7609c165cf4e7018bdd4
tracer-ics2015 <tracer@njuics.org>
2016-04-06 10:03:08 +0800

    >  compile NEMU poppy toxic Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:18:00 UTC 2015 i686 i686 i686 GNU/Linux  10:03:08 up 1 day,  7:42, 15 users,  load average: 0.08, 0.03, 0.05 75d54516c7907be285120754a77714f58463219c

--- nemu/src/monitor/debug/breakpoint.c
+++ nemu/src/monitor/debug/breakpoint.c
@@ -39,6 +39,7 @@ printf("shit!=%x\n",addr);
 }
 //first judge this is bp.If it is bp, end it.
 void end_bp(uint32_t addr){
+	printf("addr = %x/n",addr);
 	BP *temp;
 	bool isbp=1;
 	for (temp=head;temp!=free_;temp=temp->next) {
