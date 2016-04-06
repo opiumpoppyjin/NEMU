143e8fabbac1e3cb1fe30f27e21474a73aad3095
tracer-ics2015 <tracer@njuics.org>
2016-04-06 23:16:33 +0800

    >  compile NEMU poppy toxic Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:18:00 UTC 2015 i686 i686 i686 GNU/Linux  23:16:33 up 1 day, 17:06, 13 users,  load average: 0.10, 0.05, 0.05 c4c87bf6002de682d254edf3ed06bedecfa8325

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -80,7 +80,7 @@ void cpu_exec(volatile uint32_t n) {
 		if (nemu_state==STOP){
 			cpu.eip -= instr_len;
 		//	end_bp(cpu.eip);	
-		//	end_wp(cpu.eip);
+			end_wp(cpu.eip);
 		}
 		bool iswp=check_wp(cpu.eip);
 		if (iswp==true);
